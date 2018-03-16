2bd40b7f911de98e8fd674b924a37cdff26100fa
EdStrickland <2499128545@qq.com>
2018-03-16 14:58:49 +0800

    i

--- script/scripts.js
+++ script/scripts.js
@@ -1,8 +1,49 @@
 window.onload = function() {
 	var img = document.getElementById('source');
-	var can = document.getElementById('target');
-	var cxt = can.getContext('2d');
 	setInterval(function (){
-		cxt.drawImage(img, 0, 0);
-	}, 500);
+		var temp = convertCanvasToImage(convertImageToCanvas(img));
+		console.log(temp);
+	}, 20);
 }
+
+function convertImageToCanvas(image) {
+	var canvas = document.createElement("canvas");
+	canvas.width = image.width;
+	canvas.height = image.height;
+	canvas.getContext("2d").drawImage(image, 0, 0);
+
+	return canvas;
+}
+
+function convertCanvasToImage(canvas) {
+	var image = new Image();
+	image.src = canvas.toDataURL("image/png");
+	return image;
+}
+
+// var gif = new GIF({
+// 	workers: 2,
+// 	quality: 10
+// });
+// // 添加一个图片标签对象像素到当前帧
+// gif.addFrame(imageElement);
+// //或添加一个canvas对象的像素到当前帧
+// gif.addFrame(document.getElementsByTagName('canvas')[0], {
+// 	delay: 200
+// });
+// gif.addFrame(document.getElementsByTagName('canvas')[0], {
+// 	delay: 200
+// });
+// gif.addFrame(document.getElementsByTagName('canvas')[0], {
+// 	delay: 200
+// });
+// //从canvas context复制像素到当前帧
+// gif.addFrame(ctx, {
+// 	copy: true
+// });
+// //合成图片成功后
+// gif.on('finished', function(blob) {
+// 	window.open(URL.createObjectURL(blob));
+// });
+// //渲染图片
+// gif.render();
