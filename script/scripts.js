876c4c082e16c3732d98c92281c655620208cb1b
EdStrickland <2499128545@qq.com>
2018-03-17 01:27:15 +0800

    i

--- script/scripts.js
+++ script/scripts.js
@@ -39,27 +39,46 @@ window.onload = function() {
         status++;
         msg.innerText = "已下载素材：" + status + "/52";
         if (status == 52) {
-        	for (i = 0; i < 52; i++) {
+        	for (i = 1; i <= 52; i++) {
         		msg.innerText = "正在加载素材：" + i + "/52";
 	        	var temp = document.createElement("canvas");
+	        	temp.width = img.width;
+    			temp.height = img.height;
 	        	temp.getContext("2d").drawImage(dataSet[i], 0, 0);
 	        	canvases.push(temp);
 	        }
         }
+
+        msg.innerText = "准备完毕";
     }
 
     gif.on('finished', function(blob) {
         console.log("render finished");
         target.src = URL.createObjectURL(blob);
+        gif = new GIF({
+	        workers: 8,
+	        quality: 10,
+	        workerScript: '../assets/js/gif.worker.js'
+
+	    });
     });
 }
 
-function start() {
+function start(reverse) {
 	console.log(status);
 	if (status == 52) {
+		if (reverse) {
+			for (i = 51; i > -1; i--) {
+	        	msg.innerText = "正在渲染第 " + status + "/52 帧";
+	            gif.addFrame(canvases[i], { delay: 85 });
+	        }
+	        gif.render();
+	        return;
+		}
+
         for (i = 0; i < 52; i++) {
         	msg.innerText = "正在渲染第 " + status + "/52 帧";
-            gif.addFrame(canvases[i], { delay: 75 });
+            gif.addFrame(canvases[i], { delay: 85 });
         }
         gif.render();
     }
