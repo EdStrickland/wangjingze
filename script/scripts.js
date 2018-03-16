7b80af063f1c404c2799594b86e8212543b47c2b
EdStrickland <2499128545@qq.com>
2018-03-16 14:41:30 +0800

    gif testing

--- script/scripts.js
+++ script/scripts.js
@@ -1,1 +1,8 @@
-console.log('123');
+window.onload = function() {
+	var img = document.getElementById('source');
+	var can = document.getElementById('target');
+	var cxt = can.getContext('2d');
+	setInterval(function (){
+		cxt.drawImage(img, 0, 0);
+	}, 500);
+}
