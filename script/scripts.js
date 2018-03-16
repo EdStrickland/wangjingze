f37c05a3463998af6bcce8b6b96c4112443bdcd8
EdStrickland <2499128545@qq.com>
2018-03-17 00:11:35 +0800

    i

--- script/scripts.js
+++ script/scripts.js
@@ -1,43 +1,58 @@
 var dataSet = [];
+var status = 0;
 window.onload = function() {
 
-	var img = document.getElementById("source");
-	var target = document.getElementById("target");
+    var img = document.getElementById("source");
+    var target = document.getElementById("target");
 
-	var gif = new GIF({
-		workers: 2,
-		quality: 10,
-		workerScript:'../assets/js/gif.worker.js'
-	});
+    var gif = new GIF({
+        workers: 4,
+        quality: 10,
+        workerScript: '../assets/js/gif.worker.js'
+    });
 
-	console.log("render start");
+    console.log("render start");
 
-	gif.addFrame(img);
+    // gif.addFrame(img);
 
-	gif.render();
-	// for (i = 52; i >= 1; i --) {
-	// 	console.log(i);
-	// 	var src = "../assets/img/" + i + ".png";
-	// 	console.log(src);
-	// 	var img = new Image();
-	// 	img.src = src
-	// 	gif.addFrame(img);
-	// 	gif.render();
-	// }
+    // gif.render();
+    for (i = 52; i >= 1; i--) {
+        console.log(i);
+        var src = "../assets/img/" + i + ".png";
+        console.log(src);
+        var img = new Image();
+        img.src = src;
+        img.onload = function() {
+        	callback();
+        }
+        dataSet.push(img);
+    }
 
-	gif.on('finished', function(blob) {
-		console.log("render finished");
-		target.src = URL.createObjectURL(blob);
-	});
+
+
+    function callback() {
+        status++;
+        if (status == 52) {
+            for (i = 0; i < 52; i--) {
+                gif.addFrame(dataSet[i]);
+                gif.render();
+            }
+        }
+    }
+
+
+
+    gif.on('finished', function(blob) {
+        console.log("render finished");
+        target.src = URL.createObjectURL(blob);
+    });
 }
 
 function convertImageToCanvas(image) {
-	var canvas = document.createElement("canvas");
-	canvas.width = image.width;
-	canvas.height = image.height;
-	canvas.getContext("2d").drawImage(image, 0, 0);
+    var canvas = document.createElement("canvas");
+    canvas.width = image.width;
+    canvas.height = image.height;
+    canvas.getContext("2d").drawImage(image, 0, 0);
 
-	return canvas;
+    return canvas;
 }
-
-
