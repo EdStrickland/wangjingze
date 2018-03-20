10cfe3b1ba79a536742ff4795fa1dc328c37690a
Ed_Strickland <linjie.chen@cootek.cn>
2018-03-20 10:30:42 +0800

    i

--- script/scripts.js
+++ script/scripts.js
@@ -1,5 +1,6 @@
 var dataSet = [];
 var canvases = [];
+var keyFrames = [];
 var status = 0;
 var img;
 var target;
@@ -47,6 +48,7 @@ window.onload = function() {
     			var ctx = temp.getContext("2d");
 	        	ctx.drawImage(dataSet[i - 1], 0, 0);
 	        	canvases.push(temp);
+                keyFrames.push(1);
 	        }
 
         	msg.innerText = "准备完毕";
@@ -59,33 +61,39 @@ window.onload = function() {
     });
 }
 
-function start(reverse) {
+function render (canvas, text, isKeyFrame) {
+    if (!isKeyFrame)
+        return;
+    var ctx = canvases[i].getContext("2d");
+    ctx.fillStyle = "white";
+    ctx.font = "900 40px Courier New";
+    ctx.textAlign="center";
+    ctx.fillText(text, 150, 150);
+    ctx.strokeText(text, 150, 150);
+}
+
+function start (reverse) {
 	console.log(status);
 	if (status == 52) {
 		if (reverse) {
 			for (i = 51; i > -1; i--) {
-    			var ctx = canvases[i].getContext("2d");
-	        	ctx.fillStyle = "white";
-	        	ctx.font = "900 40px Courier New";
-				ctx.textAlign="center";
-	        	var text = document.getElementById("text").value;
-	        	ctx.fillText(text, 150, 150);
-	        	ctx.strokeText(text, 150, 150);
-	        	msg.innerText = "正在渲染第 " + status + "/52 帧";
-	            gif.addFrame(canvases[i], { delay: 95 });
+                var text = document.getElementById("text").value;
+                render(canvases[i], text keyFrames[i]);
+                msg.innerText = "正在渲染第 " + status + "/52 帧";
+                gif.addFrame(canvases[i], { delay: 95 });
 	        }
 	        gif.render();
 	        return;
 		}
 
         for (i = 0; i < 52; i++) {
-    			var ctx = canvases[i].getContext("2d");
-	        	ctx.fillStyle = "white";
-	        	ctx.font = "900 40px Courier New";
-				ctx.textAlign="center";
-	        	var text = document.getElementById("text").value;
-	        	ctx.fillText(text, 150, 150);
-	        	ctx.strokeText(text, 150, 150);
+			var ctx = canvases[i].getContext("2d");
+        	ctx.fillStyle = "white";
+        	ctx.font = "900 40px Courier New";
+			ctx.textAlign="center";
+        	var text = document.getElementById("text").value;
+        	ctx.fillText(text, 150, 150);
+        	ctx.strokeText(text, 150, 150);
         	msg.innerText = "正在渲染第 " + status + "/52 帧";
             gif.addFrame(canvases[i], { delay: 95 });
         }
